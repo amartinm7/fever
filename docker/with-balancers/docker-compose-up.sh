59293e1d4f8eea6196743267d85b1e6626d5d34c
antonio martin <antonio.martin@adevinta.com>
2023-07-05 10:45:04 +0200

    adding logs and docker configs

--- docker/with-balancers/docker-compose-up.sh
+++ docker/with-balancers/docker-compose-up.sh
@@ -2,3 +2,29 @@
 
 ## run three instances of the fever app
 docker-compose up -d --scale fever=3
+
+sleep 30
+
+sh ./send_messages.sh &
+
+## run three instances of the fever app
+docker-compose up -d --scale fever=5
+
+sleep 30
+
+## run three instances of the fever app
+docker-compose up -d --scale fever=2
+
+sleep 30
+
+## run three instances of the fever app
+docker-compose up -d --scale fever=1
+
+sleep 30
+
+# Stop the execution of send_messages.sh script
+pkill -f "./send_messages.sh"
+
+## run three instances of the fever app
+docker-compose down
+
